89a829bf9827b3e1f8303803aa1294283b596d64
Summertime-Nap <128077098+Summertime-Nap@users.noreply.github.com>
2024-04-17 20:39:10 -0500

    added the playround function
    playround() takes the computer and player choice
    to play a game of rps and returns a string with the outcome of the game

--- rps.js
+++ rps.js
@@ -8,3 +8,37 @@ function getComputerChoice() {
     return RPSArray[index];
 };
 
+//use the prompt method to get the players choice and store it in a variable
+
+let playerChoice = prompt('Choose between rock, paper or scissors');
+playerChoice = playerChoice.toLowerCase();
+
+//function that takes in the player and computer choice to play a single round of rps, determines a winner and outputs appropriate dialogue
+
+function playRound(computerChoice, playerChoice) {
+    if (playerChoice == computerChoice) {
+        return 'Its a tie';
+    }else if (playerChoice == 'rock') {
+        if (computerChoice == 'paper') {
+            return 'you lose! paper beats rock'
+        }else if (computerChoice == 'scissors') {
+            return 'you win! rock beat scissors'
+        }
+    }else if (playerChoice == 'paper') {
+        if (computerChoice == 'scissors') {
+            return 'you lose! scissors beats paper'
+        }else if (computerChoice == 'rock') {
+            return 'you win! paper beat rock'
+        }
+    }else if (playerChoice == 'scissors') {
+        if (computerChoice == 'rock') {
+            return 'you lose! rock beats scissors'
+        }else if (computerChoice == 'scissors') {
+            return 'you win! scissors beat paper'
+        }
+    }else {
+        return 'invalid input. please refresh the page and try agian'
+    };
+}
+
+console.log(playRound(getComputerChoice(),playerChoice));
