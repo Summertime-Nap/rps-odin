3e6d3903e4bdb558320ec1afafad6932926f0835
Summertime-Nap <128077098+Summertime-Nap@users.noreply.github.com>
2024-04-19 15:56:18 -0500

    created the playGame() function
    playGame() itterated over the playRound funtion five times to determine a winner

--- rps.js
+++ rps.js
@@ -10,35 +10,54 @@ function getComputerChoice() {
 
 //use the prompt method to get the players choice and store it in a variable
 
-let playerChoice = prompt('Choose between rock, paper or scissors');
-playerChoice = playerChoice.toLowerCase();
 
+
+let playerWins = 0;
+let playerLosses = 0;
 //function that takes in the player and computer choice to play a single round of rps, determines a winner and outputs appropriate dialogue
 
 function playRound(computerChoice, playerChoice) {
+
     if (playerChoice == computerChoice) {
         return 'Its a tie';
-    }else if (playerChoice == 'rock') {
+    } else if (playerChoice == 'rock') {
         if (computerChoice == 'paper') {
+            playerLosses++;
             return 'you lose! paper beats rock'
-        }else if (computerChoice == 'scissors') {
+        } else if (computerChoice == 'scissors') {
+            playerWins++;
             return 'you win! rock beat scissors'
         }
-    }else if (playerChoice == 'paper') {
+    } else if (playerChoice == 'paper') {
         if (computerChoice == 'scissors') {
+            playerLosses++;
             return 'you lose! scissors beats paper'
-        }else if (computerChoice == 'rock') {
+        } else if (computerChoice == 'rock') {
+            playerWins++;
             return 'you win! paper beat rock'
         }
-    }else if (playerChoice == 'scissors') {
+    } else if (playerChoice == 'scissors') {
         if (computerChoice == 'rock') {
+            playerLosses++;
             return 'you lose! rock beats scissors'
-        }else if (computerChoice == 'scissors') {
+        } else if (computerChoice == 'scissors') {
+            playerWins++;
             return 'you win! scissors beat paper'
         }
-    }else {
+    } else {
         return 'invalid input. please refresh the page and try agian'
     };
 }
 
-console.log(playRound(getComputerChoice(),playerChoice));
+
+function playGame() {
+    for (round = 1; round <= 5; round++) {
+        let playerChoice = prompt('Choose between rock, paper or scissors');
+        playerChoice = playerChoice.toLowerCase();
+        let computerChoice = getComputerChoice()
+        console.log(playRound(computerChoice, playerChoice));
+    }
+    console.log(`you won ${playerWins} times and you lost ${playerLosses} times`);
+
+}
+playGame();
